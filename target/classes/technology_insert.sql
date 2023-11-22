b47dc5ded7e81f1349f00b84391b9958fbcce020
vishwasks32 <vishwasks32@gmail.com>
2023-11-22 15:07:51 +0530

    Deploy updates

--- target/classes/technology_insert.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO technology VALUES(1,"Development");
-INSERT INTO technology VALUES(2,"Design");
-INSERT INTO technology VALUES(3,"Business Development");
-INSERT INTO technology VALUES(4,"ML and Data Science");
